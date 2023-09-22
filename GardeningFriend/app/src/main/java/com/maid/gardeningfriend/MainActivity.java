40d2c4ecbb25991e71d0e02158fc39de83ef058c
Maximilian <72700320+maxtheprogrammer98@users.noreply.github.com>
2023-09-22 17:19:32 -0300

    Se agrega menu de navegacion

--- GardeningFriend/app/src/main/java/com/maid/gardeningfriend/MainActivity.java
+++ GardeningFriend/app/src/main/java/com/maid/gardeningfriend/MainActivity.java
@@ -3,6 +3,10 @@ package com.maid.gardeningfriend;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,51 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        // inicializa el menu en la actividad
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        // valida que seccion fue seleccionada
+        if(item.getItemId() == R.id.inicio){
+            Toast.makeText(this, "inicio", Toast.LENGTH_SHORT).show();
+            return true;
+
+        } else if(item.getItemId() == R.id.enciclopedia) {
+            Toast.makeText(this, "enciclopedia", Toast.LENGTH_SHORT).show();
+            return true;
+
+        } else if(item.getItemId() == R.id.recomendaciones){
+            Toast.makeText(this, "recomendaciones", Toast.LENGTH_SHORT).show();
+            return true;
+
+        } else if(item.getItemId() == R.id.registro){
+            Toast.makeText(this, "registro", Toast.LENGTH_SHORT).show();
+            return true;
+
+        } else if(item.getItemId() == R.id.login_logout){
+            Toast.makeText(this, "login / logout", Toast.LENGTH_SHORT).show();
+            return true;
+
+        } else if(item.getItemId() == R.id.perfil){
+            Toast.makeText(this, "login / logout", Toast.LENGTH_SHORT).show();
+            return true;
+
+        } else if(item.getItemId() == R.id.panel_admin){
+            Toast.makeText(this, "login / logout", Toast.LENGTH_SHORT).show();
+            return true;
+
+        } else{
+            Toast.makeText(this, "opcion invalida", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+
     }
 }
