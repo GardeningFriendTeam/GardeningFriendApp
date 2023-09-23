fd04a44c9e3f16601291f8ccffca6eb3809bd00e
BraianOviedo <braian_oviedo@gmx.com>
2023-09-23 17:28:49 -0300

    Se implemento la autenticacion de Firebase a RegistroActivity

--- GardeningFriend/app/src/main/java/com/maid/gardeningfriend/RegistroActivity.java
+++ GardeningFriend/app/src/main/java/com/maid/gardeningfriend/RegistroActivity.java
@@ -2,16 +2,20 @@ package com.maid.gardeningfriend;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
 
+import java.util.Objects;
 import java.util.regex.Pattern;
 
 public class RegistroActivity extends AppCompatActivity {
@@ -24,12 +28,16 @@ public class RegistroActivity extends AppCompatActivity {
     private TextInputLayout textInputLayoutEmail;
     private TextInputLayout textInputLayoutPassword;
 
-    private Button buttonSignup;
+    Button buttonSignup;
+
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
 
+        // Inicialización de elementos de la interfaz de usuario
         editTextUsername = findViewById(R.id.editTextUsername);
         editTextEmail = findViewById(R.id.editTextEmail);
         editTextPassword = findViewById(R.id.editTextPassword);
@@ -39,66 +47,83 @@ public class RegistroActivity extends AppCompatActivity {
         textInputLayoutPassword = findViewById(R.id.textInputLayoutPassword);
 
         buttonSignup = findViewById(R.id.buttonSignup);
+        mAuth = FirebaseAuth.getInstance();
 
+        // Configuración del evento de clic para el botón de registro
         buttonSignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Get user input
-                String username = editTextUsername.getText().toString().trim();
-                String email = editTextEmail.getText().toString().trim();
-                String password = editTextPassword.getText().toString().trim();
+                // Obtener la entrada del usuario
+                String username = Objects.requireNonNull(editTextUsername.getText()).toString().trim();
+                String email = Objects.requireNonNull(editTextEmail.getText()).toString().trim();
+                String password = Objects.requireNonNull(editTextPassword.getText()).toString().trim();
 
-                // Validate input
+                // Validar la entrada
                 if (isValidInput(username, email, password)) {
-                    // Registration successful
-                    displayToast("Registro Exitoso!");
-                    // You can proceed with saving user data or other actions here
+                    // Registrar al usuario con Firebase
+                    mAuth.createUserWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(RegistroActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        // Registro exitoso
+                                        Toast.makeText(RegistroActivity.this, "Registro Exitoso!", Toast.LENGTH_SHORT).show();
+                                        // TODO: Después de un registro exitoso, se puede realizar acciones adicionales,
+                                        //  como iniciar una nueva Activity.
+                                    } else {
+                                        // Error durante el registro
+                                        FirebaseAuthException e = (FirebaseAuthException) task.getException();
+                                        assert e != null;
+                                        Toast.makeText(RegistroActivity.this, "Registro fallido: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
                 }
             }
         });
     }
 
-    // Function to validate user input
+    // Función para validar la entrada del usuario
     private boolean isValidInput(@NonNull String username, @NonNull String email, @NonNull String password) {
-        if (!isValidUsername(username) && !isValidEmail(email) && !isStrongPassword(password)){
-            textInputLayoutUsername.setError("El usuario debe contener minimo 3 caracteres");
-            textInputLayoutEmail.setError("Dirección de email invalida");
-            textInputLayoutPassword.setError("La contraseña debe contener minimo 8 caracteres");
-            return false;
-        }else if (!isValidUsername(username)){
-            textInputLayoutUsername.setError("El usuario debe contener minimo 3 caracteres");
-            return false;
-        }else if (!isValidEmail(email)) {
-            textInputLayoutEmail.setError("Dirección de email invalida");
-            return false;
-        }else if (!isStrongPassword(password)) {
-            textInputLayoutPassword.setError("La contraseña debe contener minimo 8 caracteres");
-            return false;
-        }else {
-            textInputLayoutUsername.setError(null);
-            textInputLayoutEmail.setError(null);
-            textInputLayoutPassword.setError(null);
+        boolean isValid = true;
 
-            return true;
+        if (!isValidUsername(username)) {
+            textInputLayoutUsername.setError("El usuario debe contener al menos 3 caracteres");
+            isValid = false;
+        } else {
+            textInputLayoutUsername.setError(null);
         }
+
+        if (!isValidEmail(email)) {
+            textInputLayoutEmail.setError("Dirección de email inválida");
+            isValid = false;
+        } else {
+            textInputLayoutEmail.setError(null);
+        }
+
+        if (!isStrongPassword(password)) {
+            textInputLayoutPassword.setError("La contraseña debe contener al menos 8 caracteres");
+            isValid = false;
+        } else {
+            textInputLayoutPassword.setError(null);
+        }
+
+        return isValid;
     }
 
-    private boolean isValidUsername(String username){
+    // Función para validar el formato del nombre de usuario
+    private boolean isValidUsername(String username) {
         return username.length() >= 3;
     }
-    // Function to validate email format using regex
+
+    // Función para validar el formato de la dirección de email utilizando regex
     private boolean isValidEmail(String email) {
         String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
         return Pattern.matches(emailPattern, email);
     }
-    // Function to check password strength
 
+    // Función para verificar la fortaleza de la contraseña
     private boolean isStrongPassword(String password) {
         return password.length() >= 8;
-    }
-
-    // Function to display a Toast message
-    private void displayToast(String message) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
