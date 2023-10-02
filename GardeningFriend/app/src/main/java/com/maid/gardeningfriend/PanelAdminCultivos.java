4e4c5b0fd42feebd8ae54bf035a22862c0e073c4
Maximilian <72700320+maxtheprogrammer98@users.noreply.github.com>
2023-10-02 18:16:43 -0300

    Se termina la primera parte del panel de admin

--- GardeningFriend/app/src/main/java/com/maid/gardeningfriend/PanelAdminCultivos.java
+++ GardeningFriend/app/src/main/java/com/maid/gardeningfriend/PanelAdminCultivos.java
@@ -1,15 +1,119 @@
 package com.maid.gardeningfriend;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.ArrayList;
+import java.util.Map;
+
+/**
+ * Aca se muestran todos los cultivos que estan en la BD
+ * para poder modificarlos / eliminarlos / agregar mas
+ */
 public class PanelAdminCultivos extends MainActivity {
+
+    //array que contiene todos los cultivos
+    ArrayList<CultivosGenerador> cultivosBD = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_panel_admin_cultivos);
+
+        // se activa la funcion que realiza el get request a la BD
+        getCultivosBD();
+
     }
+
+    /**
+     * realiza una GET REQUEST a la bd
+     * y consume todos los cultivos
+     * guardandolos en "cultivosBD"
+     */
+    private void getCultivosBD(){
+        //se obtiene instancia de la BD
+        FirebaseFirestore bd = FirebaseFirestore.getInstance();
+        // se realiza peticion
+        bd.collection("cultivos")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()){
+                            for (QueryDocumentSnapshot document : task.getResult()){
+                                // se convierte el doc iterado en un objeto
+                                Map<String, Object> data = document.getData();
+
+                                // se extraen propiedades para crear objeto
+                                String nombre = (String) data.get("nombre");
+                                String temperatura = (String) data.get("temperatura");
+                                String estacion = (String) data.get("estacion");
+                                String region = (String) data.get("region");
+                                String info = (String) data.get("informacion");
+                                String icono = (String) data.get("icono");
+                                String tipo = (String) data.get("tipo");
+                                String crecimiento = (String) data.get("crecimiento");
+
+                                // se crea un nuevo objeto para ser agregado al arraylist
+                                CultivosGenerador cultivoIterado = new CultivosGenerador(
+                                        nombre,
+                                        nombre,
+                                        tipo,
+                                        crecimiento,
+                                        info,
+                                        temperatura,
+                                        estacion,
+                                        region,
+                                        icono
+                                );
+
+                                // se llama a funcion para que agregue el cultivo a al arraylist
+                                agregarCultivo(cultivoIterado);
+                            }
+                        }else{
+                            // se muestra mensaje de error en caso que falle la conexion
+                            Toast.makeText(PanelAdminCultivos.this, "ha ocurrido un problema al conectar con la BD", Toast.LENGTH_SHORT).show();
+                        }
+
+
+                    }
+                });
+    }
+
+    /**
+     * agrega cultivo al arraylist cultivosBD
+     * (esto se hace a aparte porque la funcion que hace la peticion
+     * es asincrinica)
+     * @param cultivo
+     * instacia de CultivosGenerador
+     */
+    public void agregarCultivo(CultivosGenerador cultivo){
+        cultivosBD.add(cultivo);
+    }
+
+    /**
+     * activa el adapter, esto se realiza aparte
+     * debido a que la funcion que hace la GET REQUEST
+     * es asincronica
+     */
+    public void activarAdapter(){
+        //se identifica el recycler view
+        RecyclerView recyclerAdmin = findViewById(R.id.recycler_panel_cultivos);
+        // se crea el adapter para recyclerview
+        PanelAdminRecyclerView adapter = new PanelAdminRecyclerView(this,cultivosBD);
+        recyclerAdmin.setAdapter(adapter);
+        recyclerAdmin.setLayoutManager(new LinearLayoutManager(this));
+    }
+
 }
--- GardeningFriend/app/src/main/java/com/maid/gardeningfriend/PanelAdminRecyclerView.java
+++ GardeningFriend/app/src/main/java/com/maid/gardeningfriend/PanelAdminRecyclerView.java
@@ -1,4 +1,71 @@
 package com.maid.gardeningfriend;
 
-public class PanelAdminRecyclerView {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class PanelAdminRecyclerView extends RecyclerView.Adapter<PanelAdminRecyclerView.MyViewHolder> {
+    //atributos
+    Context context;
+    ArrayList<CultivosGenerador> cultivos;
+
+    //constructor
+    public PanelAdminRecyclerView(Context context, ArrayList<CultivosGenerador> cultivos) {
+        this.context = context;
+        this.cultivos = cultivos;
+    }
+
+    @NonNull
+    @Override
+    public PanelAdminRecyclerView.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // infla el layout con las tarjetas creadas
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.cards_cultivos_panel_admin,parent);
+
+        return new PanelAdminRecyclerView.MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PanelAdminRecyclerView.MyViewHolder holder, int position) {
+        // asigna un index a cada elem
+        // y extrae las propiedades de cada objeto del cultivo
+        holder.titulo.setText(cultivos.get(position).getNombre());
+        // a traves de la API de picasso se carga la imagen
+        Picasso.get()
+                .load(cultivos.get(position).getImagen())
+                .error(R.mipmap.logo)
+                .into(holder.icono);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        // identifica cuantos cultivos deben mostrarse
+        return cultivos.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder{
+        // basicamente crea las tarjetas de los cultivos
+        // atributos
+        ImageView icono;
+        TextView titulo;
+
+        //constructor
+        public MyViewHolder(@NonNull View itemView){
+            super(itemView);
+            icono = itemView.findViewById(R.id.ic_card_cultivos);
+            titulo = itemView.findViewById(R.id.nombre_cultivo);
+        }
+    }
+
 }
