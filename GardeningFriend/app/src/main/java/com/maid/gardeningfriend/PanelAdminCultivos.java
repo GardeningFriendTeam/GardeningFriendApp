97b2ed4d3d330da58f14643b67c54c8ffebbd01b
Maximilian <72700320+maxtheprogrammer98@users.noreply.github.com>
2023-10-02 17:19:39 -0300

    Se comienza a trabajar en el panel de admin

---
+++ GardeningFriend/app/src/main/java/com/maid/gardeningfriend/PanelAdminCultivos.java
@@ -0,0 +1,15 @@
+package com.maid.gardeningfriend;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class PanelAdminCultivos extends MainActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_panel_admin_cultivos);
+    }
+}
---
+++ GardeningFriend/app/src/main/java/com/maid/gardeningfriend/PanelAdminRecyclerView.java
@@ -0,0 +1,4 @@
+package com.maid.gardeningfriend;
+
+public class PanelAdminRecyclerView {
+}
