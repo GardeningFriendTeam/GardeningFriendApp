b4dcd1653f3410efda3ed638836a393c496d3274
BraianOviedo <braian_oviedo@gmx.com>
2023-10-04 13:30:44 -0300

    En RegistroActivity se agregaron validaciones, y se modifico los estilos, se incorporaron estilos para el Dark Mode.

--- GardeningFriend/app/src/main/java/com/maid/gardeningfriend/RegistroActivity.java
+++ GardeningFriend/app/src/main/java/com/maid/gardeningfriend/RegistroActivity.java
@@ -3,11 +3,16 @@ package com.maid.gardeningfriend;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,6 +25,10 @@ import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
@@ -32,14 +41,19 @@ public class RegistroActivity extends MainActivity {
 
     private EditText editTextName, editTextEmail, editTextPassword;
     private TextView textViewGoToLogin;
-
+    private ProgressBar progressBarRegistro;
     private FirebaseFirestore mFirestore;
     private FirebaseAuth mAuth;
+    private static final String TAG = "RegistroActivity";
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        getSupportActionBar().setTitle("Registro");
+
         mFirestore = FirebaseFirestore.getInstance();
         mAuth = FirebaseAuth.getInstance();
 
@@ -47,6 +61,17 @@ public class RegistroActivity extends MainActivity {
         editTextEmail = findViewById(R.id.editTextEmail);
         editTextPassword = findViewById(R.id.editTextPassword);
         btn_registro = findViewById(R.id.buttonSignup);
+        progressBarRegistro = findViewById(R.id.progressBarRegistro);
+        textViewGoToLogin = findViewById(R.id.textViewGoToLogin);
+
+        textViewGoToLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RegistroActivity.this, Login.class);
+                startActivity(intent);
+                Toast.makeText(RegistroActivity.this, "Login", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         btn_registro.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -55,11 +80,31 @@ public class RegistroActivity extends MainActivity {
                 String email = editTextEmail.getText().toString();
                 String password = editTextPassword.getText().toString();
 
-                if (name.isEmpty() && email.isEmpty() && password.isEmpty()){
-                    Toast.makeText(RegistroActivity.this, "Completa los campos", Toast.LENGTH_SHORT).show();
+                if (TextUtils.isEmpty(name)){
+                    Toast.makeText(RegistroActivity.this, "Completa el campo username", Toast.LENGTH_SHORT).show();
+                    editTextName.setError("Username es requerido");
+                    editTextName.requestFocus();
+                }else if (TextUtils.isEmpty(email)){
+                    Toast.makeText(RegistroActivity.this, "Completa el campo email", Toast.LENGTH_SHORT).show();
+                    editTextEmail.setError("Email es requerido");
+                    editTextEmail.requestFocus();
+                }else if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                    Toast.makeText(RegistroActivity.this, "Re-ingresa el campo email", Toast.LENGTH_SHORT).show();
+                    editTextEmail.setError("Un email valido es requerido");
+                    editTextEmail.requestFocus();
+                }else if (TextUtils.isEmpty(password)){
+                    Toast.makeText(RegistroActivity.this, "Completa el campo password", Toast.LENGTH_SHORT).show();
+                    editTextPassword.setError("Contraseña es requerida");
+                    editTextPassword.requestFocus();
+                }else if (password.length() < 8){
+                    Toast.makeText(RegistroActivity.this, "La contraseña debe tener al menos 8 digitos", Toast.LENGTH_SHORT).show();
+                    editTextPassword.setError("Contraseña muy debil");
+                    editTextPassword.requestFocus();
                 }else {
+                    progressBarRegistro.setVisibility(View.VISIBLE);
                     registerUser(name, email, password);
                 }
+//                progressBarRegistro.setVisibility(View.GONE);
             }
         });
     }
@@ -68,31 +113,51 @@ public class RegistroActivity extends MainActivity {
         mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
-                String id = mAuth.getCurrentUser().getUid();
-                Map<String, Object> map = new HashMap<>();
-                map.put("id", id);
-                map.put("name", name);
-                map.put("email", email);
-                map.put("password", password);
+                if (task.isSuccessful()){
+                    FirebaseUser firebaseUser = mAuth.getCurrentUser();
+                    String id = mAuth.getCurrentUser().getUid();
+                    Map<String, Object> map = new HashMap<>();
+                    map.put("id", id);
+                    map.put("name", name);
+                    map.put("email", email);
+                    map.put("password", password);
 
-                mFirestore.collection("user").document(id).set(map).addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void unused) {
-                        finish();
-                        startActivity(new Intent(RegistroActivity.this, Enciclopedia.class));
-                        Toast.makeText(RegistroActivity.this, "Usuario registrado con exito", Toast.LENGTH_SHORT).show();
+                    // Envia una verificación por Email
+                    firebaseUser.sendEmailVerification();
+
+                    mFirestore.collection("user").document(id).set(map).addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void unused) {
+                            // Abre el Perfil del usuario luego de un registro exitoso
+                            Intent intent = new Intent(RegistroActivity.this, Perfil.class);
+                            // Para prevenir que el usuario vuelva a RegistroActivity presionando el boton back despues de registrarse
+                            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                            startActivity(intent);
+                            finish(); // Cierra la Activity
+                            Toast.makeText(RegistroActivity.this, "Registro Exitoso! Por favor valide su Email.", Toast.LENGTH_SHORT).show();
+                            progressBarRegistro.setVisibility(View.GONE);
+                        }
+                    });
+                }else {
+                    try {
+                        throw task.getException();
+                    }catch (FirebaseAuthWeakPasswordException e){
+                        editTextPassword.setError("Tu contraseña es muy debil, " +
+                                "intenta usar una combinación de letras, números y caracteres especiales.");
+                        editTextPassword.requestFocus();
+                    }catch (FirebaseAuthInvalidCredentialsException e){
+                        editTextEmail.setError("Tu email es invalido o ya esta en uso, intenta de nuevo.");
+                        editTextEmail.requestFocus();
+                    }catch (FirebaseAuthUserCollisionException e){
+                        editTextEmail.setError("El usuario ya esta registrado con este Email, intenta con otro Email.");
+                        editTextEmail.requestFocus();
+                    }catch (Exception e){
+                        Log.e(TAG, e.getMessage());
+                        Toast.makeText(RegistroActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
                     }
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(RegistroActivity.this, "Error al guardar", Toast.LENGTH_SHORT).show();
-                    }
-                });
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Toast.makeText(RegistroActivity.this, "Error al registrar", Toast.LENGTH_SHORT).show();
+                    progressBarRegistro.setVisibility(View.GONE);
+                }
+
             }
         });
     }
