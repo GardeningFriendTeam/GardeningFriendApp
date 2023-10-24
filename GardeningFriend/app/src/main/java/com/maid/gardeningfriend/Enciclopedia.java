19fcc86ed2da8c1737ed2a188de3ecc479dd5fe8
Mariquena <allosamariquena@gmail.com>
2023-10-23 21:55:43 -0300

    Cambiar lógica

--- GardeningFriend/app/src/main/java/com/maid/gardeningfriend/Enciclopedia.java
+++ GardeningFriend/app/src/main/java/com/maid/gardeningfriend/Enciclopedia.java
@@ -1,166 +1,265 @@
 package com.maid.gardeningfriend;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.maid.gardeningfriend.favoritos.FavoritosSeccion;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
-/*
-* Pantalla principal de la enciclopedia
-* */
 public class Enciclopedia extends MainActivity implements EnciclopediaInterface{
-    EditText etBuscador;
-    Button btnBuscar;
-    String nombreIngresado;
-
-    ArrayList<CultivosGenerador> cultivos = new ArrayList<>();
+    //Atributos principales
+    SearchView searchView;
+    RecyclerView recyclerView;
+    EnciclopediaRecyclerViewAdapter adapter;
+    ArrayList<CultivosGenerador> cultivosBD = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enciclopedia);
-        // traer todos los cultivos
-        addModelsCultivos();
 
-        etBuscador = findViewById(R.id.etBuscador);
-        btnBuscar = findViewById(R.id.bBuscar);
+        getCultivosEnc();
 
+        // se activa funcionalidad del buscador
+        searchView = findViewById(R.id.buscador_enc);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String palabra) {
+                barraBusquedaEnc(palabra);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+        searchView.setOnCloseListener(new SearchView.OnCloseListener() {
+            @Override
+            public boolean onClose() {
+                cultivosBD.clear();
+                getCultivosEnc();
+                return false;
+            }
+        });
 
     }
 
-    /*
-    * Agrega los cultivos
-    * */
-    private void addModelsCultivos() {
-        // 1- se crea una instancia de la BD para acceder a la colección
+    /**
+     * realiza una get request a la BD
+     * para extraer todos los cultivos
+     */
+    private void getCultivosEnc(){
+        //instancia de Firestore
         FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-        // 2- se realiza un get request para consumir los datos de los cultivos
-        db.collection("cultivos").get()
-                // verifica si la petición fue exitosa
+        //mensajes de alerta
+        Toast msjExito = Toast.makeText(Enciclopedia.this,
+                "conexion exitosa a la BD",
+                Toast.LENGTH_SHORT);
+
+        Toast msjError = Toast.makeText(Enciclopedia.this,
+                "ocurrio un error al conectar con la BD",
+                Toast.LENGTH_SHORT);
+
+        //se realiza peticion
+        db.collection("cultivos")
+                .get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
                         if (task.isSuccessful()){
-                            Log.i("tag","petición exitosa");
-                            // se recorre la coleccion y se extraen los datos necesarios
+                            // se itera sobre cada doc de la coleccion
                             for (QueryDocumentSnapshot document : task.getResult()) {
-                                // se guarda la info consumida en un objeto map
-                                Map<String, Object> data = document.getData();
+                                // se convierta cada documento en un haspmap para poder manipularlo
+                                Map<String, Object> cultivoDocument = document.getData();
 
-                                // se extraen las propiedades del elem iterado
-                                String nombre = (String) data.get("nombre");
-                                String temperatura = (String) data.get("temperatura");
-                                String estacion = (String) data.get("estacion");
-                                String region = (String) data.get("region");
-                                String informacion = (String) data.get("informacion");
-                                String crecimiento = (String) data.get("crecimiento");
-                                String tipo = (String) data.get("tipo");
-                                String imagen = (String) data.get("icono");
+                                String ID = (String) document.get("id");
+                                String nombre = (String) document.get("nombre");
+                                String tipo = (String) document.get("tipo");
+                                String info = (String) document.get("informacion");
+                                String crecimiento = (String) document.get("crecimiento");
+                                String icono = (String) document.get("icono");
+                                String temperatura = (String) document.get("temperatura");
+                                String estacion = (String) document.get("estacion");
+                                String region = (String) document.get("region");
 
-                                // se crea objeto para agregar cultivo al array
-                                CultivosGenerador nuevoCultivo = new CultivosGenerador(
-                                        nombre,
+                                //se crea un objeto de tipo "cultivo"
+                                CultivosGenerador cultivo = new CultivosGenerador(
+                                        ID,
                                         nombre,
                                         tipo,
                                         crecimiento,
-                                        informacion,
+                                        info,
                                         temperatura,
                                         estacion,
                                         region,
-                                        imagen);
-
-                                agregarCultivo(nuevoCultivo);
-
+                                        icono
+                                );
+                                addCultivo(cultivo);
                             }
-                            // una vez que se iteran todos los documentos se activa el recyclerview adapter
-                            activarAdapter();
-
-                            Log.i("tag", "tamaño array: " + cultivos.size());
-                        } else {
-                            // fracaso de la peticion & se muestra mensajes
-                            Toast.makeText(Enciclopedia.this,"Hubo un error al conectarse con la BD", Toast.LENGTH_SHORT).show();
+                            setAdapter();
                         }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
                     }
                 });
     }
 
-    private void agregarCultivo(CultivosGenerador cultivo) {
-        cultivos.add(cultivo);
-        Log.i("tag", "cultivo agreagado: " + cultivo.nombre);
-    }
     /**
-     * activa el adapter de recycler view
-     * para pasar todos los cultivos
-     * guardados en el array "cultivos"
+     * agrega el cultivo al array
+     * que luego se pasa al adapter
+     * @param cultivoExtraido
+     * cultivo extraido de la BD
      */
-    public void activarAdapter(){
-        //se identifica el recyclerview de la activity
-        RecyclerView rv = findViewById(R.id.rvcultivosf);
+    private void addCultivo(CultivosGenerador cultivoExtraido){
+        cultivosBD.add(cultivoExtraido);
+        Log.i("enc", "cultivo añadido: " + cultivoExtraido.getNombre());
+    }
 
-        // se activa el "adapter" para que pase las tarjetas al recycler
-        EnciclopediaRecyclerView adapter = new EnciclopediaRecyclerView(this,cultivos,this);
-        rv.setAdapter(adapter);
-        rv.setLayoutManager(new LinearLayoutManager(this));
+    /**
+     * activa el adaptador y pasa el array de refe
+     */
+    private void setAdapter(){
+        // se identifica el recyclerview
+        recyclerView = findViewById(R.id.enc_recyclerview);
+        // se activa su adaptador
+        adapter = new EnciclopediaRecyclerViewAdapter(this, cultivosBD, this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
     @Override
     public void onItemClick(int position) {
         // 1 - se crea intent para dirigir al user a la pantalla con info extra:
-        Intent intent = new Intent(Enciclopedia.this, EnciclopediaDetalles.class);
+        Intent intent = new Intent(Enciclopedia.this, RecomendacionesDetalles.class);
 
         // 2 - se crea objeto parceable para pasar las propiedades del cult selec a la siguiente
         //pantalla
         CultivosDetallesParceable detallesCultivo = new CultivosDetallesParceable(
-                cultivos.get(position).getNombre(),
-                cultivos.get(position).getTemperatura(),
-                cultivos.get(position).getEstacionSiembra(),
-                cultivos.get(position).getRegion(),
-                cultivos.get(position).getCaracteristicas(),
-                cultivos.get(position).getImagen(),
-                cultivos.get(position).getTipo(),
-                cultivos.get(position).getDuracionCrecimiento()
+                cultivosBD.get(position).getNombre(),
+                cultivosBD.get(position).getTemperatura(),
+                cultivosBD.get(position).getEstacionSiembra(),
+                cultivosBD.get(position).getRegion(),
+                cultivosBD.get(position).getCaracteristicas(),
+                cultivosBD.get(position).getImagen(),
+                cultivosBD.get(position).getTipo(),
+                cultivosBD.get(position).getDuracionCrecimiento()
         );
 
+        //prueba
+        Log.i("tag", "nombre cultivo: " + detallesCultivo.getNombreCultivo());
+
+        // 3 - se pasa el objeto parceable al intent
+        intent.putExtra("CULTIVO_DETALLES", detallesCultivo);
+
+        // 4  - se inicialza la nueva actividad (intent)
+        startActivity(intent);
     }
 
-    public void buscarCultivos(View v) {
-        // toast que muestra mensaje de error (en caso de ocurrir):
-        Toast toast = Toast.makeText(Enciclopedia.this, "No ingresaste texto", Toast.LENGTH_SHORT);
+    private void barraBusquedaEnc(String input){
+        //mensajes de alerta
+        Toast msjExito = Toast.makeText(Enciclopedia.this,
+                "resultados de busqueda: ",
+                Toast.LENGTH_SHORT);
 
-        nombreIngresado = etBuscador.getText().toString().trim();
-        if(!nombreIngresado.isEmpty()){
-            // 1 - se crea intent
-            Intent intent = new Intent(Enciclopedia.this, EnciclopediaCultivos.class);
+        Toast msjError = Toast.makeText(Enciclopedia.this,
+                "no se encontraron resultados",
+                Toast.LENGTH_SHORT);
 
-            // 2 - se crea un objeto parceable que contiene los valores que se pasaran
-            // a la siguiente activity (EnciclopediaCultivos)
-            CultivosEnciParceable datosEnciclopedia = new CultivosEnciParceable(nombreIngresado);
+        Toast errorBD = Toast.makeText(Enciclopedia.this,
+                "error al conectar con la BD",
+                Toast.LENGTH_SHORT);
 
-            // 3 - se pasa el objeto parceable
-            intent.putExtra("nombre", nombreIngresado);
-            startActivity(intent);
+        // 1 - se crea instancia de BD
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-        }else {
-            toast.show();
+        // 2 - se valida que la palabra este en favoritos
+        Log.i("arrayfav", cultivosBD.toString());
+
+        boolean flagInput = false;
+        for (CultivosGenerador cultivo : cultivosBD){
+            if(cultivo.getNombre().equals(input.toLowerCase())){
+                flagInput = true;
+            }
         }
+
+        // 3 - se realiza la get request si la flag esta activada
+        if(flagInput){
+            // se limpia el array del adapter primero
+            cultivosBD.clear();
+
+            // se realiza otra get request para actualizar el array del adapter
+            db.collection("cultivos")
+                    .whereEqualTo("nombre", input)
+                    .get()
+                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            if(task.isSuccessful()){
+                                for (QueryDocumentSnapshot documentSnapshot : task.getResult() ) {
+                                    // se extrae cada objeto iterado en un hashmap
+                                    Map<String, Object> cultivoDoc = documentSnapshot.getData();
+
+                                    String ID = (String) cultivoDoc.get("id");
+                                    String nombre = (String) cultivoDoc.get("nombre");
+                                    String tipo = (String) cultivoDoc.get("tipo");
+                                    String info = (String) cultivoDoc.get("informacion");
+                                    String icono = (String) cultivoDoc.get("icono");
+                                    String temperatura = (String) cultivoDoc.get("temperatura");
+                                    String estacion = (String) cultivoDoc.get("estacion");
+                                    String region = (String) cultivoDoc.get("region");
+                                    String crecimiento = (String) cultivoDoc.get("crecimiento");
+
+                                    // se crea un nuevo objeto "cultivo" en base a la info extraida
+                                    CultivosGenerador cultivo = new CultivosGenerador(
+                                            ID,
+                                            nombre,
+                                            tipo,
+                                            crecimiento,
+                                            info,
+                                            temperatura,
+                                            estacion,
+                                            region,
+                                            icono
+                                    );
+                                    Log.i("sarchbarget", "se agrega cultivo que coincide con la busqueda");
+                                    addCultivo(cultivo);
+                                }
+                                msjExito.show();
+                                // se notifica de los cambios al adapter
+                                adapter.notifyDataSetChanged();
+                            }
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            msjError.show();
+                            Log.w("errorBD", "error al conectar con la BD", e);
+                        }
+                    });
+        }
+
+
     }
-
-
-
 }
