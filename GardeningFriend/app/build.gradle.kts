3e1c04e50eec8c3c9e5113bd0e3158f89c2ee7a6
Maximilian <72700320+maxtheprogrammer98@users.noreply.github.com>
2024-05-21 16:24:09 -0300

    Agregando UI de seccion IA

--- GardeningFriend/app/build.gradle.kts
+++ GardeningFriend/app/build.gradle.kts
@@ -5,12 +5,13 @@ plugins {
 
 android {
     namespace = "com.maid.gardeningfriend"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.maid.gardeningfriend"
         minSdk = 24
-        targetSdk = 33
+        //noinspection EditedTargetSdkVersion
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -48,4 +49,5 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("com.google.guava:guava:30.0-jre")
 }
---
+++ GardeningFriend/app/src/main/java/com/maid/gardeningfriend/seccionIA/ActivitySeccionIA.java
@@ -0,0 +1,16 @@
+package com.maid.gardeningfriend.seccionIA;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.maid.gardeningfriend.R;
+
+public class ActivitySeccionIA extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seccion_ia);
+    }
+}
