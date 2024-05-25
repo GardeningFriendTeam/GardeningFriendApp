7551b052af108c6258beab2f103dea3f40d69827
Maximilian <72700320+maxtheprogrammer98@users.noreply.github.com>
2024-05-25 14:08:29 -0300

    Ultimas mejoras - seccion IA

--- GardeningFriend/app/src/main/java/com/maid/gardeningfriend/seccionIA/ActivityShowResponse.kt
+++ GardeningFriend/app/src/main/java/com/maid/gardeningfriend/seccionIA/ActivityShowResponse.kt
@@ -1,14 +1,17 @@
 package com.maid.gardeningfriend.seccionIA
 
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
+import com.maid.gardeningfriend.MainActivity
 import com.maid.gardeningfriend.R
+import com.squareup.picasso.Picasso
 
-class ActivityShowResponse : AppCompatActivity() {
+class ActivityShowResponse : MainActivity() {
     // setting properties
     var text: TextView? = null;
     var model: ModelRespuestaIA? = null;
+    var image: ImageView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +22,13 @@ class ActivityShowResponse : AppCompatActivity() {
 
         // displaying content in textView
         text = findViewById(R.id.text_show_ia_response)
+        image = findViewById(R.id.ic_plant_ia)
         text!!.text = model!!.texto
+
+        // using picasso to render image
+        Picasso.get()
+            .load(model!!.imagenLink)
+            .error(R.mipmap.logo)
+            .into(image)
     }
 }
