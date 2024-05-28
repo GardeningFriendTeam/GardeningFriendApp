c9e0acc9af935fc3dd22ca1218ea7aa263543de8
Cristian Valles <valles1710@gmail.com>
2024-05-28 16:27:14 -0300

    Se agregan funcionalidades a la seccion Recetas

--- GardeningFriend/app/src/main/java/com/maid/gardeningfriend/recetas/RecetasActivity.java
+++ GardeningFriend/app/src/main/java/com/maid/gardeningfriend/recetas/RecetasActivity.java
@@ -2,15 +2,67 @@ package com.maid.gardeningfriend.recetas;
 
 import android.os.Bundle;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.maid.gardeningfriend.MainActivity;
 import com.maid.gardeningfriend.R;
 
-public class RecetasActivity extends AppCompatActivity {
+public class RecetasActivity extends MainActivity {
+
+    RecyclerView recyclerViewRecetas;
+
+    private static final String TAG = "RecetasActivity";
+    RecetasAdapter recetasAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recetas);
+
+        // Obtener referencias a elementos de la interfaz de usuario
+        recyclerViewRecetas = findViewById(R.id.recycler_recetas);
+
+        // Configura y muestra las recetas en el RecyclerView
+        setupRecyclerView();
+
+    };
+
+    // Configura el RecyclerView para mostrar las recetas
+    void setupRecyclerView() {
+        // Obtener una consulta de Firestore para las recetas
+        Query query = FirebaseFirestore.getInstance().collection("recetas");
+        // Configurar las opciones del adaptador para Firebase Firestore
+        FirestoreRecyclerOptions<Receta> options = new FirestoreRecyclerOptions.Builder<Receta>()
+                .setQuery(query, Receta.class).build();
+        // configurar el administrador de diseño y el adaptador para el RecyclerView
+        recyclerViewRecetas.setLayoutManager(new LinearLayoutManager(this));
+        recetasAdapter = new RecetasAdapter(options, this);
+        recyclerViewRecetas.setAdapter(recetasAdapter);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        recetasAdapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (recetasAdapter != null) {
+            recetasAdapter.stopListening();
+        }
+    }
+
+    protected void onResume() {
+        super.onResume();
+        if (recetasAdapter != null) {
+            recetasAdapter.notifyDataSetChanged();
+        }
+    }
+
 }
--- GardeningFriend/app/src/main/java/com/maid/gardeningfriend/recetas/RecetasDetailsActivity.java
+++ GardeningFriend/app/src/main/java/com/maid/gardeningfriend/recetas/RecetasDetailsActivity.java
@@ -1,16 +1,55 @@
 package com.maid.gardeningfriend.recetas;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.maid.gardeningfriend.R;
+import com.squareup.picasso.Picasso;
 
 public class RecetasDetailsActivity extends AppCompatActivity {
+
+    TextView recetaNombreTextView, recetaIngredientesTextView,
+            recetaCookTimeTextView, recetaInstruccionesTextView,
+            recetaCategoriaTextView;
+    ImageView recetaImageViewDetails;
+    private String docId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recetas_details);
+
+        // obtener referencias a los elementos de la interfaz
+        recetaNombreTextView = findViewById(R.id.textViewRecipeName);
+        recetaIngredientesTextView = findViewById(R.id.textViewIngredients);
+        recetaCookTimeTextView = findViewById(R.id.textViewCookTime);
+        recetaInstruccionesTextView = findViewById(R.id.textViewInstructions);
+        recetaCategoriaTextView = findViewById(R.id.textViewCategory);
+        recetaImageViewDetails = findViewById(R.id.imageViewRecipeDetails);
+
+        Intent intent = getIntent();
+        if (intent != null) {
+            String nombre = intent.getStringExtra("nombre");
+            String cookTime = intent.getStringExtra("cookTime");
+            String ingredientes = intent.getStringExtra("ingredientes");
+            String instrucciones = intent.getStringExtra("instrucciones");
+            String categoria = intent.getStringExtra("categoria");
+            String imageUrl = intent.getStringExtra("imageUrl");
+
+            recetaNombreTextView.setText(nombre);
+            recetaCookTimeTextView.setText("tiempo de elaboracion: " + cookTime);
+            recetaIngredientesTextView.setText("Ingredientes: \n" + ingredientes);
+            recetaInstruccionesTextView.setText("Instrucciones: \n" + instrucciones);
+            recetaCategoriaTextView.setText("Categoria: " + categoria);
+
+            Picasso.get().load(imageUrl).into(recetaImageViewDetails);
+        }
+
     }
+
+
 }
